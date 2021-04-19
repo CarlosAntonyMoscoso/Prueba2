d8edca039dfa61507704d299f36cb83bd0a70426
CARLITOS <CARLITOS@DESKTOP-DN009DA>
2021-04-19 21:22:10 +0200

    Tabla multiplicar

--- src/PruebaDos/Prueba.java
+++ src/PruebaDos/Prueba.java
@@ -1,20 +1,47 @@
 package PruebaDos;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Prueba {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		FileWriter fw;
+		String miFichero = "C:\\Users\\CARLITOS\\Desktop\\fichero\\TablaMultiplicar.txt";
+		BufferedWriter bw;
+		String linea = "";
+	
 
-		System.out.println("Hola mundo");
-		System.out.println("hol");
-		System.out.println("adios");
-		
-		System.out.println("Asereje");
-		System.out.println("De aqui a la lnasa");
-		
-		System.out.println("Holaaaaaaaaaaaaaaaaaaa");
-		System.out.println("adios");
-		System.out.println("que tal");
-		System.out.println("adiooooooos");
+		try {
+			fw = new FileWriter(miFichero);
+			bw = new BufferedWriter(fw);
+
+			for (int i = 1; i <= 10; i++) {
+				linea = "";
+				bw.write("tabla del :" + i);
+				bw.newLine();
+				for (int j = 1; j <= 10; j++) {
+					linea += j + "x" + i + "=" + (i * j) + "\n";
+					
+				}
+				System.out.println(linea);
+				bw.write(linea);
+				bw.newLine();
+			}
+			
+			
+			
+			bw.close();
+
+			
+		} catch (IOException e) {
+			System.out.println("Error al escribir");
+		}
+
 	}
 
 }
